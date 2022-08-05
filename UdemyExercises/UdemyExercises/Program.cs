0895d1ec77558f7201d5f73fe10d3aba659525f8
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-05 09:54:45 -0500

    Exercise parsing c#

---
+++ UdemyExercises/UdemyExercises/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UdemyExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForFloat = "0.85";
+            string stringForInt = "12345";
+            float float1 = float.Parse(stringForFloat);
+            int int1 = Int32.Parse(stringForInt); 
+            Console.WriteLine(stringForFloat);
+            Console.WriteLine(stringForInt);
+        }
+    }
+}
